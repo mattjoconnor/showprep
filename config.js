9cbc468ba0796fc29338d977a5827212d8b58405
mattjoconnor <mattoconnorpost@gmail.com>
2026-03-25 15:18:59 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// ShowPrep config
+const SP_KEY_ENCRYPTED = 'Fwl1eWDo7T4mSLZ3FeKWkTaFbyaTMYJikJ/L1vnEwR055cbPzEQcBqIEy+URngd6cMdc11hXel5B2bk1Bo/9txT72pdFm2OLbt1/a5pvRq6hqfNRR/bs9Q==';
+const SP_BIN = '69c42b75aa77b81da91beea0';
