8990a66fe674a27249c56a01c3dac63b1f2735d8
mattjoconnor <mattoconnorpost@gmail.com>
2026-03-26 11:59:41 -0400

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// ShowPrep config
-const SP_KEY_ENCRYPTED = 'WP1ED4BS6m0Y0hEDIAaCyrj7r+TwI6kIMZcp4XsB3zaHvodWW9J/zCxnywrxXvwzKPioZPgjk7BOhIJYswNvPyVBVDe412ueD08fLd+iRTARynwENONWcQ==';
-const SP_BIN = '69c42b75aa77b81da91beea0';
